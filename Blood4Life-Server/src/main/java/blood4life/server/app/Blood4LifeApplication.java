c457a3b9f12b01c314f3232fcd0e089f6f21be21
Tuldrum <90346628+Tuldrum@users.noreply.github.com>
2022-03-04 12:40:47 -0500

    construccion exes

--- Blood4Life-Server/src/main/java/blood4life/server/app/Blood4LifeApplication.java
+++ Blood4Life-Server/src/main/java/blood4life/server/app/Blood4LifeApplication.java
@@ -1,10 +1,15 @@
 package blood4life.server.app;
 
-import blood4life.server.infra.Blood4LifeServerSocket;
+import blood4life.server.domain.services.GestorServicios;
+import blood4life.server.infra.Blood4LifeHandler;
+import blood4life.serversocket.serversockettemplate.infra.ServerSocketMultiThread;
 
 public class Blood4LifeApplication {
     public static void main(String args[]){
-        Blood4LifeServerSocket server = new Blood4LifeServerSocket();
-        server.startServer();
+        ServerSocketMultiThread myServer = new ServerSocketMultiThread(3000);
+        Blood4LifeHandler myHandler = new Blood4LifeHandler();
+        myHandler.setService(new GestorServicios());
+        myServer.setServerHandler(myHandler);
+        myServer.startServer();
     }
 }
